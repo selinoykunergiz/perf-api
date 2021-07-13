064e7b88937209c80ca0b058a61d921b5e49d8c0
Selin Öykü Nergiz <49616975+selinoykunergiz@users.noreply.github.com>
2021-07-13 20:45:34 +0300

    Perf API :: Missed comment area

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,4 @@
 module.exports = {
-  //url: "mongodb+srv://speedDB:Selin55;@cloud.te1os.mongodb.net/speedDatabase?retryWrites=true&w=majority"
-  //url: "mongodb+srv://"+process.env.DB_USERNAME+":"+process.env.DB_PASSWORD+"@"+process.env.DB_URL+"/"+process.env.DB_NAME+"?retryWrites=true&w=majority"
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   url: process.env.DB_URL,
